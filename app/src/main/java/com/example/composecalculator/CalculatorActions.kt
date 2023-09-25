d91663ad7560fa8e5ec999d0f39fbd5a561be599
billyyan <By869422>
2023-09-25 00:13:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/composecalculator/CalculatorActions.kt
@@ -0,0 +1,9 @@
+package com.example.composecalculator
+
+sealed class CalculatorAction {
+    data class Number(val number: Int) : CalculatorAction()
+    object Clear : CalculatorAction()
+    object Delete : CalculatorAction()
+    object Decimal: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+}
